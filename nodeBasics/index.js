e9739006c71bb78be7c48fee23a728c8f802a1fb
LiliiaMoskovchuk <123palamar@gmail.com>
2026-04-24 00:49:43 +0300

    added Node.js project files to the repository

---
+++ nodeBasics/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+console.log(`
+STATUS: ${chalk.blueBright.bold('LEARNING JS...')}
+TIME REQUIRED: ${chalk.greenBright('100 years')}
+PROGRESS: ${chalk.magentaBright('0.7%')}
+MOTIVATION: ${chalk.cyanBright('still alive')}
+`);
